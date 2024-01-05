f62578eef6a2587c5b5c87b0787121497f15e885
utsavxf <utb4578@gmail.com>
2024-01-05 23:36:30 +0530

    updated resume 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
                   Explore my latest projects and articles, showcasing my expertise in React.js and web development.
                 </p>
                 <div className='flex items-center self-start mt-2 lg:self-center'>
-                  <Link href="https://drive.google.com/drive/folders/14UYZt10YpJReTRs-UDDWtrGouqf4vjL7" target={"_blank"}
+                  <Link href="https://drive.google.com/file/d/1Aah-mBVRnMr3YtcWiG1X18b6jFf0m8rW/view?usp=drive_link" target={"_blank"}
                   className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base ' 
                   download={true}
                  >Resume
